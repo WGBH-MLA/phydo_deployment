58a9039188272df5c16761847ad4ef97c94776dc
Andrew Myers <afredmyers@gmail.com>
2017-02-03 13:16:50 -0700

    Renames env vars to use AAPB instead of PHYDO

--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -1,12 +1,12 @@
-raise ArgumentError, "Missing required environment variable, AAPB_HOST." unless ENV['AAPB_HOST']
-raise ArgumentError, "Missing required environment variable, AAPB_SSH_KEY." unless ENV['AAPB_SSH_KEY']
+raise ArgumentError, "Missing required environment variable, PHYDO_HOST." unless ENV['PHYDO_HOST']
+raise ArgumentError, "Missing required environment variable, PHYDO_SSH_KEY." unless ENV['PHYDO_SSH_KEY']
 
-server ENV['AAPB_HOST'],
+server ENV['PHYDO_HOST'],
   user: 'ec2-user',
   roles: %w{web app db},
   ssh_options: {
     user: 'ec2-user',
-    keys: [ENV['AAPB_SSH_KEY']],
+    keys: [ENV['PHYDO_SSH_KEY']],
     forward_agent: false,
     auth_methods: %w(publickey)
   }
